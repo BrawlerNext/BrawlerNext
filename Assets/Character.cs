727908b4544c55a18355b3f658ef850bf8f60696
Antonio S�nchez Le�n <antonio_sl93@hotmail.com>
2017-12-08 20:36:45 +0000

    Added basic schema

--- Assets/Character.cs
+++ Assets/Character.cs
@@ -1,16 +1,43 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Character : MonoBehaviour {
+public abstract class Character : MonoBehaviour {
 
-	// Use this for initialization
 	void Start () {
 		
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		
+		// Movement
+
+		// Skills
+		switch (getKeyKode()) {
+			case KeyCode.Mouse0:
+				SoftAttackSkill();
+			break;
+			case KeyCode.Mouse1:
+				HardAttackSkill();
+			break;
+			case KeyCode.Space:
+				JumpSkill();
+			break;
+		}
 	}
+
+	public abstract void SoftAttackSkill();
+	public abstract void HardAttackSkill();
+	public abstract void JumpSkill();
+
+    private KeyCode getKeyKode()
+    {
+		foreach (KeyCode keyCode in System.Enum.GetValues(typeof(KeyCode)))
+		{
+			if (keyCode.ToString() == Input.inputString)
+				return keyCode;
+		}
+
+		return KeyCode.Mouse0;
+    }
 }
