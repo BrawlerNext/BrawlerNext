b1b8f5991c261bcc694464998b237b42c8b91691
Jesé Romero Arbelo <jeseromeroarbelo@gmail.com>
2018-03-14 17:01:45 +0000

    Added platform movement

---
+++ Assets/Scripts/util/PlatformMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMovement : MonoBehaviour {
+
+    public Vector3 initialPosition;
+    public Vector3 finalPosition;
+
+    protected bool startToEnd = true;
+
+    private void Awake() {
+        this.transform.position = initialPosition;
+    }
+
+    private void Update()
+    {
+        Vector3 currentPosition = this.transform.position;
+
+        Vector3 newPos;
+        bool changeDirection;
+
+        if (startToEnd)
+        {
+            newPos = Vector3.Lerp(currentPosition, finalPosition, Time.deltaTime);
+
+            changeDirection = Mathf.Abs(Vector3.Distance(currentPosition, finalPosition)) < 0.5f;
+        }
+        else
+        {
+            newPos = Vector3.Lerp(currentPosition, initialPosition, Time.deltaTime);
+
+            changeDirection = Mathf.Abs(Vector3.Distance(currentPosition, initialPosition)) < 0.5f;
+        }
+
+        this.transform.position = newPos;
+
+        if (changeDirection)
+            {
+                startToEnd = !startToEnd;
+            }
+
+        
+    }
+}
