9d446e2c2474293da46c136a45b1d8f67ee1176a
Jesé Romero Arbelo <jeseromeroarbelo@gmail.com>
2018-05-02 19:49:18 +0100

    Added menu interactions

---
+++ Assets/Scripts/MenuCameraMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuCameraMovement : MonoBehaviour {
+    public GameObject characterSelection;
+    public GameObject mapSelection;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       
+	}
+
+    public void FadePositionToCharacterSelection()
+    {
+        this.transform.position = characterSelection.transform.position;
+    }
+
+    public void FadePositionToMapSelection()
+    {
+        this.transform.position = mapSelection.transform.position;
+    }
+}
